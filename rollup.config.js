e0db37de31b607448c2c5bb1536b311d06ea489a
Pasqualino Sorice <lino.sorice@gmail.com>
2018-08-23 18:49:58 +0200

    add version to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,18 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import { version } from './package.json'
 
 export default {
   input: 'index.js',
   output: [
     {
-      file: 'dist/atomax-connector.js',
+      file: `dist/atomax-connector.${version}.js`,
       name: 'AtomaxConnector',
       format: 'iife'
     },
     {
-      file: 'dist/atomax-connector-es.js',
+      file: `dist/atomax-connector-es.${version}.js`,
       format: 'es'
     }
   ],

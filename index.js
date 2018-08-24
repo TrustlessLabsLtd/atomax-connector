8838e8ef8bf5e2f83e7d4f2cccc29c59bcdf8129
gabrielem_ <gabriele.marazzi@gmail.com>
2018-08-24 09:59:00 +0200

    Change some variable name and add TODO

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 import Connector from 'qrcode-generator'
 
-const createConnectorImg = async ({name, to, value, gasPrice = null, gasLimit = null, nonce = null, data = null, size = 5, padding = 10, addressCB, txIdCB}) => {
+/*
+  TODO: change variables like "data", "size", "padding"
+  with more descritive one. 
+*/
+
+const createConnectorImg = async ({connectorName, to, ethQty, gasPrice = null, gasLimit = null, nonce = null, data = null, size = 5, padding = 10, addressCB, txIdCB}) => {
   var config = {
     apiKey: 'AIzaSyAAS3GK4zF6ZFpDYZuBuF5HCATEyBL8m4w',
     authDomain: 'whitelist-8a24c.firebaseapp.com',
@@ -14,49 +19,58 @@ const createConnectorImg = async ({name, to, value, gasPrice = null, gasLimit = 
 
   firebase.initializeApp(config)
 
-  let sessionId = window.localStorage[name]
+  let sessionId = window.localStorage[connectorName]
 
-  if (!sessionId || !(await existsOnFirebase(sessionId)) || !(await sameDataOnFirebase(sessionId, to, value, gasPrice, gasLimit, nonce, data))) {
+  if (!sessionId || !(await existsOnFirebase(sessionId)) || !(await sameDataOnFirebase(sessionId, to, ethQty, gasPrice, gasLimit, nonce, data))) {
     sessionId = firebase.database().ref('sessions').push({
       tx: {
         to,
-        value,
+        ethQty,
         gasPrice,
         gasLimit,
         nonce,
         data
       }
     }).key
-    window.localStorage.setItem(name, sessionId)
+    window.localStorage.setItem(connectorName, sessionId)
   }
 
   const connectorImg = new Connector(0, 'H')
   connectorImg.addData('atomax-connector:' + sessionId)
   connectorImg.make()
 
-  startSocket(name, sessionId, addressCB, txIdCB)
+  startSocket(connectorName, sessionId, addressCB, txIdCB)
 
   return connectorImg.createSvgTag(size, padding)
 }
 
 const existsOnFirebase = async (sessionId) => {
+  /*
+    TODO: change this variable "value" in ethQty
+  */
   const session = (await firebase.database().ref(`sessions/${sessionId}`).once('value')).val()
   return !!session
 }
 
+/*
+  TODO: change this variable "value" in ethQty
+*/
 const sameDataOnFirebase = async (sessionId, to, value, gasPrice, gasLimit, nonce, data) => {
   const tx = (await firebase.database().ref(`sessions/${sessionId}/tx`).once('value')).val()
   return tx.to === to && tx.value === value && tx.gasPrice === gasPrice && tx.gasLimit === gasLimit && tx.nonce === nonce && tx.data === data
 }
 
-const startSocket = (name, sessionId, addressCB, txIdCB) => {
+/*
+  TODO: change this variable "value" in ethQty
+*/
+const startSocket = (connectorName, sessionId, addressCB, txIdCB) => {
   firebase.database().ref(`sessions/${sessionId}/address`).on('value', (snapshot) => {
     addressCB(snapshot.val())
   })
   firebase.database().ref(`sessions/${sessionId}/tx`).on('value', (snapshot) => {
     const tx = snapshot.val()
     if (tx && tx.id) {
-      window.localStorage.removeItem(name)
+      window.localStorage.removeItem(connectorName)
     }
     txIdCB(tx)
   })
